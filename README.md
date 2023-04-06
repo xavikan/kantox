e801a19dd7115ef740ba7e7567dfb5e4ddd4fdb8
xavikan <xavi.gonzalez.kantox@gmail.com>
2023-04-06 16:56:32 +0200

    1 st commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Kantox - QA Test V2.1
 
 Xavi Gonzalez Arriola
+
+INSTALL
+-------
+npm init
+npm install cypress --save-dev
+./node_modules/.bin/cypress open        // first run of cypress to set it up
---
+++ cypress/e2e/kantox_test.cy.js
@@ -0,0 +1,33 @@
+/* describe('Vendor Search', function () {
+    context('UserBundle/Desktop/Users/UserMenu', function () {
+        
+        beforeEach(function () {
+            cy.visit("www.google.es")
+        })
+
+        it('"Before you continue to Google Search" disclaimer is shown on first visit', function () {
+            cy.get('#S3BnEe').should('be.visible')                // check if element which the message "Before you continue to Google Search" is shown
+                                                                  // this looks te be valid for any language
+        })
+
+        it('Google main page is shown as we accept the disclaimer', function () {
+            cy.get('[class="QS5gu sy4vM"]').eq(1).click()         // click "Accept"
+            cy.get('img[class="lnXdpd"]').should('be.visible')    // double check, by class
+            cy.get('img[alt="Google"]').should('be.visible')      // and by alt
+        })
+
+        it('Search field can be X as something is typed', function () {
+            cy.get('[class="QS5gu sy4vM"]').eq(1).click()         // click "Accept"
+            cy.get('.gLFyf').type('1234')                         // type something
+            cy.get('.ExCKkf > svg').click()                       // click delete X
+            cy.get('.gLFyf').should('be.empty')                   // check it gets empty
+        })  
+
+    }) */
+
+    describe('template spec', () => {
+        it('passes', () => {
+          cy.visit(".\model.html");
+        })
+      })
+//})
